14194dae216995b927aec6fefbb4b5d4846ee0c4
Martin Fedorenko <67556470+Martin-Fedorenko@users.noreply.github.com>
2025-06-06 14:40:20 -0300

    Comienzo del proyecto

---
+++ src/main/java/mycfo/MycfoApplication.java
@@ -0,0 +1,13 @@
+package mycfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycfoApplication.class, args);
+	}
+
+}
