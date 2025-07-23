5e437bb9dbc71342358d81bf781952b1ec56f3ea
Martin Fedorenko <MartinFedorenkoLK1288@gmail.com>
2025-07-23 20:19:07 -0300

    estructura inicial del proyecto

---
+++ consolidacion/src/main/java/consolidacion/ConsolidacionApplication.java
@@ -0,0 +1,13 @@
+package consolidacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolidacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsolidacionApplication.class, args);
+	}
+
+}
