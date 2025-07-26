604a92434c63566cd085e0eeb9b93821d7fa554a
Martin Fedorenko <MartinFedorenkoLK1288@gmail.com>
2025-07-26 00:09:17 -0300

    probando react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import SignIn from './sign-in/SignIn';
+import SignUp from './sign-up/SignUp';
 import Dashboard from './dashboard/Dashboard';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
