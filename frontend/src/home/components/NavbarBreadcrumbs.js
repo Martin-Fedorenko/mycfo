982d8f06fc639d375f2e1fe7a30f1b70a0f44f54
Erik <eotero@frba.utn.edu.ar>
2025-11-24 01:52:52 -0300

    Fix color iconos de la navbar, estaban grises y los textos negros, ahora todo igual

--- frontend/src/home/components/NavbarBreadcrumbs.js
+++ frontend/src/home/components/NavbarBreadcrumbs.js
@@ -66,7 +66,7 @@ export default function NavbarBreadcrumbs({ sx }) {
       {icon
         ? React.cloneElement(icon, {
             fontSize: 'small',
-            sx: { color: 'text.secondary' },
+            sx: { color: 'text.primary' },
           })
         : null}
       <Typography
